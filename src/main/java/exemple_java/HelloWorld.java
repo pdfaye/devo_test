a83d8d59d7e8b8ef1b483e8b795ce577a6511613
pdfaye <papadaoudafaye995@gmail.com>
2019-11-06 16:14:48 +0100

    Premier commit

---
+++ src/main/java/exemple_java/HelloWorld.java
@@ -0,0 +1,13 @@
+package exemple_java;
+
+public class HelloWorld {
+	public String getMessage() {
+		String message="Hello World";
+		System.out.print(message);
+		return message;
+	}
+	public int sum(int a, int b) {
+		return a+b;
+	}
+
+}
