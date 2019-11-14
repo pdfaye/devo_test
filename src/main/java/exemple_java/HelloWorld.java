dbe17f20d3870848d76daec89b67d51c19a16b03
pdfaye <papadaoudafaye995@gmail.com>
2019-11-14 14:59:18 +0100

    Deuxieme commite

--- src/main/java/exemple_java/HelloWorld.java
+++ src/main/java/exemple_java/HelloWorld.java
@@ -1,12 +1,16 @@
 package exemple_java;
 
 public class HelloWorld {
-	public String getMessage() {
-		String message="Hello World";
-		System.out.print(message);
-		return message;
+	
+	public String getMessage(String name) {
+		if (name!= null && !name.isEmpty()) {
+			return "Hello "+name+"!";
+		} else {
+			return "Hello World!";
+		}
 	}
-	public int sum(int a, int b) {
+
+	public int sum(int a, int b){
 		return a+b;
 	}
 
---
+++ src/test/java/exemple_java/HelloWorldTest.java
@@ -0,0 +1,39 @@
+package exemple_java;
+
+import static org.junit.Assert.assertEquals;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+public class HelloWorldTest {
+
+	private HelloWorld subject;
+
+	@Before
+	public void setup() {
+		subject = new HelloWorld();
+	}
+	
+	@Test
+	public void testGetMessage() {
+		assertEquals("Hello World!", subject.getMessage(null));
+	}
+
+	@Test
+	public void testGetMessage2() {
+		assertEquals("Hello Tonux!", subject.getMessage("Tonux"));
+	}
+
+	@Test
+	public void testSum(){
+		assertEquals(4, subject.sum(2,2));
+	}
+
+	@Test
+	public void testSum2(){
+		assertNotEquals(5, subject.sum(1,3));
+	}
+	
+}
